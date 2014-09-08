c6dd1cb55107ded6762d1ccaa912b593151c2649
BoppreH <boppreh@gmail.com>
2014-09-08 19:17:08 -0300

    Add algorithm specification in module docs

--- aes.py
+++ aes.py
@@ -5,6 +5,39 @@ https://github.com/bozhu/AES-Python
 
 PKCS#7 padding, CBC mode, PKBDF2, HMAC, byte array and string support added 
 by Lucas Boppre (http://boppreh.com) at https://github.com/boppreh/aes
+
+
+Although this is an exercise, the `encrypt` and `decrypt` functions should
+provide reasonable security to encrypted messages. The algorithm is as follows:
+
+
+1) 16 random bytes of salt are extracted from the system's secure random number
+generator (usually /dev/urandom)>
+
+2) The given key is stretched by PKBDF2-HMAC(SHA256) using the salt from 1),
+to generate the AES key, HMAC key and IV (initialization vector for CBC).
+
+3) The given message is encrypted with AES-128 using the AES key and IV from
+step 2), in CBC mode, using PKCS#7 padding.
+
+4) A HMAC-SHA256 is generated from the concatenation of the salt from 1) and
+the ciphertext from 3).
+
+5) The final ciphertext is HMAC + salt + ciphertext.
+
+salt <- random(16)
+key_aes, key_hmac, iv <- PKBDF2(master_key, salt)
+HMAC(salt + E_key_aes(message, iv)) + salt + E_key_aes(message, iv)
+
+
+Security overview:
+
+- The random salt ensures the same message will map to different ciphertexts.
+
+- The HMAC ensures the integrity of both the entire ciphertext and the PKBDF2
+salt; encrypt-then-mac prevents attacks like Padding Oracle.
+
+- Bytes from keys, iv and salt are not reused in different algorithms.
 """
 
 

3d5d8b29c8fa92659e39ff6675949b54608ca5ac
BoppreH <boppreh@gmail.com>
2014-09-15 15:55:06 -0300

    Add assert of padding len > 0

--- aes.py
+++ aes.py
@@ -307,6 +307,7 @@ class AES:
 
         plaintext = b''.join(blocks)
         padding_len = plaintext[-1]
+        assert padding_len > 0
         message, padding = plaintext[:-padding_len], plaintext[-padding_len:]
         assert all(p == padding_len for p in padding)
         return message
